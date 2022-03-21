6e70385de63ed79ff5ec2741d63a10a87cc49027
jallow101 <jallow101@users.noreply.github.com>
2022-03-21 00:23:07 +0000

    Banner Intro

--- src/Banner.js
+++ src/Banner.js
@@ -9,15 +9,26 @@ const Banner = () => {
             <span className="relative text-white text-3xl ">Yaay Salon</span>
           </span>
         </blockquote>
-        
-
         <div className="flex flex-col justify-center">
-          <h2 className="text-3xl text-pink-400 text-center mt-20">
+          <h2 className="text-3xl font-bold text-pink-400 text-center mt-20 mb-4">
             Welcome to Yaay Salon
           </h2>
-          <p className="text-md text-blue-300 text-center">The hands of perfection laced with a twist of heaven</p>
-         
+          <p className="text-lg text-blue-300 text-center">
+            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block text-white">
+              <span className="relative text-white text-3xl ">
+                The hands of perfection
+              </span>
+            </span>
+
+            <span className="before:block before:absolute before:-inset-1 before:-skew-x-12 before:bg-blue-300 relative inline-block text-white">
+              <span className="relative text-white text-3xl before:-skew-x-6 ">
+              laced with a twist from heaven
+              </span>
+            </span>
+
+          </p>
         </div>
+        
       </div>
     </div>
   );
